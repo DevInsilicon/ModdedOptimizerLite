ed346626f0f64553a462a0e59ae0e9c2cf5dad1c
Jack <jackbaker321@icloud.com>
2024-08-31 17:55:40 -0500

    Skibidi

--- src/client/java/dev/insilicon/moddedoptimizerlite/client/mixin/ClientConnectionMixin.java
+++ src/client/java/dev/insilicon/moddedoptimizerlite/client/mixin/ClientConnectionMixin.java
@@ -4,8 +4,12 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.item.*;
+import net.minecraft.entity.decoration.EndCrystalEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
@@ -14,41 +18,46 @@ import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
 public class ClientConnectionMixin {
+
     @Inject(method = "send(Lnet/minecraft/network/packet/Packet;)V", at = @At("HEAD"))
-    private void onPacketSend(Packet<?> packet, CallbackInfo info) {
+    private void onPacketSend(Packet<?> packet, CallbackInfo ci) {
         final MinecraftClient mc = MinecraftClient.getInstance();
-        if (packet instanceof PlayerInteractEntityC2SPacket) {
-            PlayerInteractEntityC2SPacket interactPacket = (PlayerInteractEntityC2SPacket)packet;
+        if (packet instanceof PlayerInteractEntityC2SPacket interactPacket) {
             interactPacket.handle(new PlayerInteractEntityC2SPacket.Handler() {
+                @Override
                 public void interact(Hand hand) {
+                    // Empty implementation
                 }
 
+                @Override
                 public void interactAt(Hand hand, Vec3d pos) {
+                    // Empty implementation
                 }
 
+                @Override
                 public void attack() {
                     HitResult hitResult = mc.crosshairTarget;
                     if (hitResult != null) {
-                        if (hitResult.getType() == HitResult.Type.ENTITY) {
-                            EntityHitResult entityHitResult = (EntityHitResult)hitResult;
-                            Entity entity = entityHitResult.getEntity();
-                            if (entity instanceof MobEntity) {
-                                StatusEffectInstance weakness = mc.player.getStatusEffect(StatusEffects.WEAKNESS);
-                                StatusEffectInstance strength = mc.player.getStatusEffect(StatusEffects.STRENGTH);
-                                if (weakness != null && (strength == null || strength.getAmplifier() <= weakness.getAmplifier()) && !ClientConnectionMixin.this.isTool(mc.player.getMainHandStack())) {
-                                    return;
-                                }
-
-                                entity.kill();
-                                entity.setRemoved(Entity.RemovalReason.KILLED);
-                                entity.onRemoved();
+                        Entity entity;
+                        if (hitResult.getType() == HitResult.Type.ENTITY && (entity = ((EntityHitResult)hitResult).getEntity()) instanceof EndCrystalEntity) {
+                            StatusEffectInstance weakness = null;
+                            if (mc.player != null) {
+                                weakness = mc.player.getStatusEffect(StatusEffects.WEAKNESS);
                             }
+                            StatusEffectInstance strength = mc.player.getStatusEffect(StatusEffects.STRENGTH);
+                            if (weakness != null && (strength == null || strength.getAmplifier() <= weakness.getAmplifier()) && !isTool(mc.player.getMainHandStack())) {
+                                return;
+                            }
+                            entity.kill();
+                            entity.setRemoved(Entity.RemovalReason.KILLED);
+                            entity.onRemoved();
                         }
                     }
                 }
@@ -56,9 +65,10 @@ public class ClientConnectionMixin {
         }
     }
 
+    @Unique
     private boolean isTool(ItemStack itemStack) {
-        if (itemStack.getItem() instanceof ToolItem && !(itemStack.getItem() instanceof SwordItem)) {
-            ToolMaterial material = ((ToolItem)itemStack.getItem()).getMaterial();
+        if (itemStack.getItem() instanceof MiningToolItem && !(itemStack.getItem() instanceof SwordItem)) {
+            ToolMaterial material = ((MiningToolItem) itemStack.getItem()).getMaterial();
             return material == ToolMaterials.NETHERITE || material == ToolMaterials.DIAMOND;
         } else {
             return false;
